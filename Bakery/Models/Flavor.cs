37e30484222173d40129c1ed8df99768920ce57a
Kymani Stephens <kymanist00@gmail.com>
2023-06-03 14:59:00 -0700

    update Flavor class properties

---
+++ Bakery/Models/Flavor.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; 
+
+namespace Factory.Models
+{
+  public class Flavor
+  {
+    public int FlavorId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public List<FlavorTreat> FlavorTreats { get; set; } // JOIN TABLE
+    // public ApplicationUser User { get; set; }
+  }
+}
