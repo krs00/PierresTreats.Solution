1991e4b46b802f405f9daf3a158c16d2fc827315
Kymani Stephens <kymanist00@gmail.com>
2023-06-03 14:45:37 -0700

    add Treat class properties

---
+++ Bakery/Models/Treat.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; 
+
+namespace Factory.Models
+{
+  public class Treat
+  {
+    public int TreatId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public List<FlavorTreat> FlavorTreats { get; set; } // JOIN TABLE
+    // public ApplicationUser User { get; set; }
+
+
+  }
+}
